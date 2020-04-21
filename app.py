62c163b3e761b600c6dec20c45ac870ecf24e27c
Sotunbo David <david.sotunbo@e4email.net>
2020-04-21 14:18:42 +0100

    update on the code

--- app.py
+++ app.py
@@ -2,4 +2,11 @@
 # @Author: sotunbo
 # @Date:   2020-04-19 10:00:55
 # @Last Modified by:   sotunbo
-# @Last Modified time: 2020-04-19 10:00:55
+# @Last Modified time: 2020-04-21 13:52:29
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
+CORS(app)
---
+++ db.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: sotunbo
+# @Date:   2020-04-19 10:02:01
+# @Last Modified by:   sotunbo
+# @Last Modified time: 2020-04-19 10:02:01
